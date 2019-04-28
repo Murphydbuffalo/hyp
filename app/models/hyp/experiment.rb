e759c16cc79402ad07e29d913f73e9f8532432f6
Dan Murphy <murphydbuffalo@gmail.com>
2019-04-27 18:25:02 -0600

    Validate range of values for control and MDE

--- app/models/hyp/experiment.rb
+++ app/models/hyp/experiment.rb
@@ -3,6 +3,8 @@ module Hyp
     has_many  :alternatives, foreign_key: 'hyp_experiment_id', dependent: :destroy
     validates :alpha, inclusion: { in: [0.05, 0.01] }
     validates :power, inclusion: { in: [0.80, 0.90] }
+    validates :control, numericality: { less_than_or_equal_to: 1.0, greater_than_or_equal_to: 0.0 }
+    validates :minimum_detectable_effect, numericality: { less_than_or_equal_to: 1.0, greater_than_or_equal_to: 0.0 }
     validates_uniqueness_of :name
   end
 end
