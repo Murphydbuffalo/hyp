0c14350b3923abff96c12f304980f794bb0c6a6f
Dan Murphy <murphydbuffalo@gmail.com>
2019-06-13 16:28:22 -0600

    Generator for installing initializer, routes, and migrations

---
+++ lib/generators/hyp/install_generator.rb
@@ -0,0 +1,31 @@
+module Hyp
+  module Generators
+    class InstallGenerator < Rails::Generators::Base
+      class_option 'db-interface', type: :string, default: 'active_record'
+      source_root Hyp::Engine.root
+
+      def create_initializer
+        create_file 'config/initializers/hyp.rb', "Hyp.db_interface = :#{options['db-interface']}"
+      end
+
+      def mount_engine
+        route "mount Hyp::Engine => '/hyp'"
+      end
+
+      def copy_migrations
+        return unless options['db-interface'] == 'active_record'
+
+        Dir[Hyp::Engine.root.join("db/migrate/*.rb")].each_with_index do |migration, i|
+          name      = migration.split('/').last.split('_').slice(1..-1).join('_')
+          timestamp = (Time.current.strftime('%Y%m%d%H%M%S').to_i + i).to_s
+
+          if Rails.root.join("db/migrate/*#{name}").exist?
+            puts "Migration #{name} has already been copied to your app"
+          else
+            copy_file migration, "db/migrate/#{timestamp}_#{name}"
+          end
+        end
+      end
+    end
+  end
+end
