46d39b8e27583a701882887fb9e3743b8b761922
Dan Murphy <murphydbuffalo@gmail.com>
2019-06-14 14:05:35 -0600

    Make user class configurable from install generator.

--- lib/generators/hyp/install_generator.rb
+++ lib/generators/hyp/install_generator.rb
@@ -2,10 +2,16 @@ module Hyp
   module Generators
     class InstallGenerator < Rails::Generators::Base
       class_option 'db-interface', type: :string, default: 'active_record'
+      class_option 'user-class',   type: :string, default: 'User'
       source_root Hyp::Engine.root
 
       def create_initializer
-        create_file 'config/initializers/hyp.rb', "Hyp.db_interface = :#{options['db-interface']}"
+        file_contents = <<-FILE
+Hyp.db_interface = :#{options['db-interface']}
+Hyp.user_class_name = '#{options['user-class']}'
+        FILE
+
+        create_file 'config/initializers/hyp.rb', file_contents
       end
 
       def mount_engine
