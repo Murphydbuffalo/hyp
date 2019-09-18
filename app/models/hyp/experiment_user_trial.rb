4dca689e02fec85818b0148fd9a5f574ecd194bc
Dan Murphy <hello@danmurphy.codes>
2019-09-18 13:20:45 -0600

    Get rid of unwanted `class_name` key.
    In `ExperimentUserTrial` association

--- app/models/hyp/experiment_user_trial.rb
+++ app/models/hyp/experiment_user_trial.rb
@@ -3,9 +3,9 @@ module Hyp
     class ExperimentUserTrial < ApplicationRecord
       extend Hyp::ExperimentUserTrialHelpers::Callbacks
 
-      belongs_to :experiment,  foreign_key: 'hyp_experiment_id'
-      belongs_to :variant, foreign_key: 'hyp_variant_id'
-      belongs_to :user,        foreign_key: Hyp.user_foreign_key_name, class_name: Hyp.user_class_name
+      belongs_to :experiment, foreign_key: 'hyp_experiment_id'
+      belongs_to :variant,    foreign_key: 'hyp_variant_id'
+      belongs_to :user,       foreign_key: Hyp.user_foreign_key_name, class_name: Hyp.user_class_name
     end
   elsif Hyp.db_interface == :mongoid
     class ExperimentUserTrial
@@ -13,9 +13,9 @@ module Hyp
       include Mongoid::Timestamps
       extend Hyp::ExperimentUserTrialHelpers::Callbacks
 
-      belongs_to :experiment,  class_name: 'Hyp::Experiment', foreign_key: 'hyp_experiment_id'
-      belongs_to :variant, class_name: 'Hyp::Variant', foreign_key: 'hyp_variant_id'
-      belongs_to :user,        class_name: 'Hyp::ExperimentUserTrial', foreign_key: Hyp.user_foreign_key_name, class_name: Hyp.user_class_name
+      belongs_to :experiment, class_name: 'Hyp::Experiment',   foreign_key: 'hyp_experiment_id'
+      belongs_to :variant,    class_name: 'Hyp::Variant',      foreign_key: 'hyp_variant_id'
+      belongs_to :user,       class_name: Hyp.user_class_name, foreign_key: Hyp.user_foreign_key_name
 
       field :converted, type: Boolean, default: false
 
