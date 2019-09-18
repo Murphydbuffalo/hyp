f7b905a677529e28e0f0bc521ea938a3c5aa4983
Dan Murphy <hello@danmurphy.codes>
2019-09-18 13:21:11 -0600

    Don't `return` from a callback.
    Causes local jump errors when invoked outside of a method, eg in an
    RSpec spec.

--- app/models/hyp/experiment_user_trial_helpers/callbacks.rb
+++ app/models/hyp/experiment_user_trial_helpers/callbacks.rb
@@ -5,7 +5,7 @@ module Hyp
         extending_class.instance_eval(
           <<-RUBY
             after_create do
-              return unless Hyp.experiment_complete_callback.respond_to?(:call) && experiment.finished?
+              next unless Hyp.experiment_complete_callback.respond_to?(:call) && experiment.finished?
               Hyp.experiment_complete_callback.call(experiment_id)
             end
           RUBY
