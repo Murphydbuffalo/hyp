6554b0c14e5ab8929f6ac95c2d9c0bcd13a9c041
Dan Murphy <hello@danmurphy.codes>
2019-10-06 17:00:38 -0600

    Add some sanity check CRUD specs

--- spec/dummy/spec/controllers/hyp_experiments_controller_spec.rb
+++ spec/dummy/spec/controllers/hyp_experiments_controller_spec.rb
@@ -3,9 +3,11 @@
 require 'rails_helper'
 
 describe Hyp::ExperimentsController, controller: true do
+  include ActionView::Helpers::NumberHelper
   routes { Hyp::Engine.routes }
+  render_views
 
-  let!(:exp1) do
+  let(:exp1) do
     Hyp::ExperimentRepo.create(
       alpha: 0.05,
       power: 0.80,
@@ -15,7 +17,7 @@ describe Hyp::ExperimentsController, controller: true do
     )
   end
 
-  let!(:exp2) do
+  let(:exp2) do
     Hyp::ExperimentRepo.create(
       alpha: 0.01,
       power: 0.90,
@@ -26,7 +28,10 @@ describe Hyp::ExperimentsController, controller: true do
   end
 
   describe 'GET #index' do
-    render_views
+    before do
+      exp1
+      exp2
+    end
 
     it 'returns a paginated list of experiments' do
       get :index
@@ -43,6 +48,45 @@ describe Hyp::ExperimentsController, controller: true do
 
       expect(response.body).not_to include exp1.name
       expect(response.body).to include exp2.name
+    end
+  end
+
+  describe 'GET #show' do
+    before do
+      exp1
+    end
+
+    it 'returns a detailed view of an experiment' do
+      get :show, params: { id: exp1.id }
+      expect(response.body).to include exp1.name
+      expect(response.body).to include exp1.alpha.to_s
+      expect(response.body).to include exp1.power.to_s
+      expect(response.body).to include exp1.control.to_s
+      expect(response.body).to include exp1.minimum_detectable_effect.to_s
+      expect(response.body).to include number_with_delimiter(exp1.sample_size)
+    end
+  end
+
+  describe 'GET #new' do
+    it 'returns a detailed view of an experiment' do
+      get :new
+      expect(response.body).to include 'Learn more about sample size calculations.'
+    end
+  end
+
+  describe 'POST #create' do
+    it 'creates a new experiment' do
+      expect {
+        post :create, params: {
+          experiment: {
+            alpha: 0.05,
+            power: 0.80,
+            control: 0.12,
+            minimum_detectable_effect: 0.02,
+            name: 'A wonderful experiment'
+          }
+        }
+      }.to change { Hyp::Experiment.count }.by(1)
     end
   end
 end
