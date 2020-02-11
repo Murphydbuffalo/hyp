22fc42e8a21ea04301eafd279dc5a2a9eb32d6a3
Dan Murphy <hello@danmurphy.codes>
2020-02-11 13:42:33 -0700

    Override mounting application's CSP in `Hyp::ApplicationController`
    So that we can load JS and CSS from Fontawesome.

--- app/controllers/hyp/application_controller.rb
+++ app/controllers/hyp/application_controller.rb
@@ -1,5 +1,11 @@
 module Hyp
   class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
+
+    content_security_policy do |p|
+      p.script_src :self, :unsafe_inline, '*.fontawesome.com'
+      p.style_src :self, :unsafe_inline, '*.fontawesome.com'
+      p.font_src :self, '*.fontawesome.com'
+    end
   end
 end
